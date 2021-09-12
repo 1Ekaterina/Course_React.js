5402909411cf46d2d0ccfdb0761a7b90e14a6412
Ekaterina <kh.katerina@bk.ru>
2021-09-12 12:45:27 +0300

    all work 1

---
+++ project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App(props) {
+ return (
+   <div className="App">
+     <header className="App-header">
+       zdec` text
+       <h3>Hello, my name is {props.name}</h3>
+     </header>
+   </div>
+ );
+}
+
+export default App;
