3ea20dec76f113526cbf50c6b33f0b73487414b2
Ekaterina <kh.katerina@bk.ru>
2021-10-04 11:26:19 +0300

    work6

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,22 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Chat from "./Chat";
-import Profile from "./Profile";
-import AppBar from "./AppBar";
+import Router from "./Components/Router/Router";
 
-const App = () => {
-  return (
-    <Router>
-      <AppBar />
-      <Switch>
-        <Route path="/chat">
-          <Chat />
-        </Route>
-        <Route path="/Profile">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+function App() {
+  return <Router />;
+}
 
 export default App;
---
+++ new-app/src/Components/InputForm/InputForm.js
@@ -0,0 +1,52 @@
+import { memo, useEffect, useRef, useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import "./inputForm.css";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
+import addMessage from "../../action/messagesActions";
+
+function InputForm({ chatId }) {
+  const inputRef = useRef();
+  const [textValue, setTextValue] = useState("");
+  const author = useSelector((state) => state.profile.name, shallowEqual);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [textValue]);
+
+  function submitHandler(e) {
+    e.preventDefault();
+    dispatch(addMessage(chatId, textValue, author));
+    setTextValue("");
+  }
+
+  return (
+    <form className="form">
+      <TextField
+        sx={{
+          width: 500,
+        }}
+        id="filled-basic-small"
+        label="Сообщение"
+        variant="filled"
+        size="small"
+        className="form__text"
+        inputRef={inputRef}
+        type="text"
+        value={textValue}
+        onChange={(e) => setTextValue(e.target.value)}
+      ></TextField>
+      <Button
+        variant="contained"
+        color="success"
+        type="submit"
+        className="form__submit"
+        onClick={(e) => submitHandler(e)}
+      >
+        Отправить
+      </Button>
+    </form>
+  );
+}
+
+export default memo(InputForm);
